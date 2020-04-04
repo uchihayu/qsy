0793fee207d6180faf8e37468d164815720e4372
shengjiayu <shengjiayu@qingsuyun.com>
2020-04-04 09:43:57 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function Hello(props) {
+  return <p>Hello, {props.name}</p>;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Hello name={"Betty"}></Hello>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
